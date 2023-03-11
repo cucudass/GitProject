4a33df4161b07004c241ab2dea7f051861198f95
조광래 <whrhkdfo12@naver.com>
2023-03-11 17:20:34 +0900

    fixed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int count = 20;
+            int count = 30;
             Console.WriteLine("Hello World!");
         }
     }
